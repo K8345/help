562b0c6d48dd1856cb536eea07b8301de8eaa417
aismeg <iaskja20@gmai.com>
2025-01-30 00:29:45 +0100

    help

---
+++ help.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const image = document.getElementById("help");
+    image.style.opacity = 1;
+
+    let clickCount = 0;
+    const maxClicks = 15;
+    const stretchFactor = 0.6;
+
+    image.addEventListener("click", function () {
+        if (clickCount < maxClicks) {
+            image.style.transform = `scaleX(${1 + (clickCount + 1) * stretchFactor / maxClicks})`;
+            clickCount++;
+        } else {
+            image.style.transform = "scaleX(1)";
+            clickCount = 0;
+        }
+    });
+});
